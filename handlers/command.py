d14a2b276666c4ac0ddc97f4ab624fad833e5229
Никита Дерюгин <deryugin@nanosemantics.ai>
2023-07-29 18:47:12 +0300

    Чекнул logger

--- handlers/command.py
+++ handlers/command.py
@@ -1,7 +1,7 @@
 """Модуль обработчиков команд."""
 from telegram import Update
 from telegram.ext import ContextTypes
-
+from log.logger import logger
 from markups.markups import start as start_markup
 
 
@@ -18,5 +18,9 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         f"Привет {update.effective_user.name} "
         f"я бот конвертер, с моими возможностями "
         f"можно ознакомиться в пункете Помощь"
+    )
+    logger.info(
+        f"User with name: {update.effective_user.name} "
+        f"and id {update.effective_user.id} executed a start"
     )
     await update.message.reply_text(text, reply_markup=start_markup())
